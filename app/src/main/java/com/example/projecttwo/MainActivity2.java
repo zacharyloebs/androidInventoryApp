a27003124e085b83ae50bc633c7abf39d204fda8
zacharyloebs <zacharyloebs@gmail.com>
2020-08-11 16:56:51 -0700

    Redesigning second activity and layout

--- app/src/main/java/com/example/projecttwo/MainActivity2.java
+++ app/src/main/java/com/example/projecttwo/MainActivity2.java
@@ -1,111 +1,73 @@
 package com.example.projecttwo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
+
+public class MainActivity2 extends AppCompatActivity {
 
     DatabaseHelper db;
     private Button buttonAdd;
-    private LinearLayout layoutList;
-    private EditText editItem;
-    private EditText editQuantity;
+    private ListView listView;
+    private EditText editItem, editQuantity;
     private ImageView imageClose;
+    int quantity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        layoutList = findViewById(R.id.layout_list);
-        buttonAdd = findViewById(R.id.button_add);
-        buttonAdd.setOnClickListener(this);
-
-        addView();
-
+        buttonAdd = findViewById(R.id.buttonAdd);
         editItem = findViewById(R.id.editTextTextItem);
+        editQuantity = findViewById(R.id.editTextTextQuantity);
 
-    }
+        db = new DatabaseHelper(this);
 
-    public void onClick(View view) {
-        addView();
-    }
-
-    private void addView() {
-
-        final View inventoryView = getLayoutInflater().inflate(R.layout.row_add_item, null,false);
-
-        editItem = inventoryView.findViewById(R.id.editTextTextItem);
-        editQuantity = inventoryView.findViewById(R.id.editTextTextQuantity);
-        imageClose = inventoryView.findViewById(R.id.image_close);
-
-        editItem.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                boolean check = db.createItem(editItem.getText().toString().trim(), editQuantity.getText().toString().trim());
-                if (check == true) {
-                    Toast.makeText(getApplicationContext(), "Successfully Added Item", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Item Already Exists",Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        imageClose.setOnClickListener(new View.OnClickListener() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                removeView(inventoryView);
+                if (validate()) {
+                    // Save data to database
+                    boolean insert = db.createUsers(editItem.getText().toString().trim(), editQuantity.getText().toString().trim());
+                    if (insert == true) {
+                        Toast.makeText(getApplicationContext(), "Successfully Added Item",Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Item Already Exists",Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
-
-        layoutList.addView(inventoryView);
-    }
-
-    private void removeView(View view) {
-
-        layoutList.removeView(view);
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        
     }
     private Boolean validate() {
         boolean result = false;
 
-        String name = editItem.getText().toString();
-        String pass = editQuantity.getText().toString();
+        String item = editItem.getText().toString().trim();
+        String quantity = editQuantity.getText().toString().trim();
+        boolean isNumber = Pattern.matches("[0-9]+", quantity);
 
-        if(name.isEmpty() && pass.isEmpty()) {
-            Toast.makeText(this, "Please Enter All Fields", Toast.LENGTH_SHORT).show();
-        }else{
+        if (item.isEmpty() && quantity.isEmpty()) {
+            Toast.makeText(this, "Both Fields Are Empty", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.isEmpty()) {
+            Toast.makeText(this, "Please Add An Item", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (isNumber == false) {
+            Toast.makeText(this, "Enter A Valid Number", Toast.LENGTH_SHORT).show();
+
+        } else {
             result = true;
         }
         return result;
     }
-
 }
